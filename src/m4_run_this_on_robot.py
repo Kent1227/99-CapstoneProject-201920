cee1dd2e5d2edc51fe9c7cb69c9fbcac8a7d4879
mahne <mahnee@rose-hulman.edu>
2019-02-07 15:10:53 -0500

    m4 drive color tests

--- src/m4_run_this_on_robot.py
+++ src/m4_run_this_on_robot.py
@@ -25,12 +25,45 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    # arm_tests()
+    drive_color_tests()
+    #     Color tests are [Black, Red, Brown, White]
+    # drive_distance_tests()
+
+def drive_color_tests():
+    run_drive_greater_intensity_tests()
+    run_drive_lesser_intensity_tests()
+    run_drive_color_tests()
+    run_drive_not_color_tests()
+
+def run_drive_greater_intensity_tests():
+    robot = rosebot.RoseBot()
+    robot.drive_system.go_straight_until_intensity_is_greater_than(45,100)
+
+def run_drive_lesser_intensity_tests():
+    robot = rosebot.RoseBot()
+    robot.drive_system.go_straight_until_intensity_is_less_than(30,35)
+
+def run_drive_color_tests():
+    robot = rosebot.RoseBot()
+    robot.drive_system.go_straight_until_color_is("Black",50)
+    robot.drive_system.go_straight_until_color_is("Red", 50)
+    robot.drive_system.go_straight_until_color_is("Brown", 50)
+    robot.drive_system.go_straight_until_color_is("White", 50)
+
+def run_drive_not_color_tests():
+    robot = rosebot.RoseBot()
+    robot.drive_system.go_straight_until_color_is_not("Black", 50)
+    robot.drive_system.go_straight_until_color_is_not("Red", 50)
+    robot.drive_system.go_straight_until_color_is_not("Brown", 50)
+    robot.drive_system.go_straight_until_color_is_not("White", 50)
+
+def arm_tests():
     run_test_calibrate_arm()
     time.sleep(2)
     run_test_arm()
     time.sleep(2)
     run_test_move_arm_to_position()
-
 
 def run_test_calibrate_arm():
     robot = rosebot.RoseBot()
