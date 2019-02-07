d44db503c2d540475f209f0b35704bc09de1c9b7
mannanej <mannanej@rose-hulman.edu>
2019-02-07 14:20:46 -0500

    I implemented the different drive functions.

--- src/rosebot.py
+++ src/rosebot.py
@@ -74,15 +74,25 @@ class DriveSystem(object):
 
     def go(self, left_wheel_speed, right_wheel_speed):
         """ Makes the left and right wheel motors spin at the given speeds. """
+        self.left_motor.turn_on(left_wheel_speed)
+        self.right_motor.turn_on(right_wheel_speed)
 
     def stop(self):
         """ Stops the left and right wheel motors. """
+        self.left_motor.turn_off()
+        self.right_motor.turn_off()
 
     def go_straight_for_seconds(self, seconds, speed):
         """
         Makes the robot go straight (forward if speed > 0, else backward)
         at the given speed for the given number of seconds.
         """
+        start = time.time()
+        self.go(speed, speed)
+        while True:
+            if time.time() - start >= seconds:
+                self.stop()
+                break
 
     def go_straight_for_inches_using_time(self, inches, speed):
         """
@@ -90,6 +100,9 @@ class DriveSystem(object):
         for the given number of inches, using the approximate
         conversion factor of 10.0 inches per second at 100 (full) speed.
         """
+        seconds_per_inch = 10.0
+        seconds = abs(inches * seconds_per_inch / speed)
+        self.go_straight_for_seconds(seconds, speed)
 
     def go_straight_for_inches_using_encoder(self, inches, speed):
         """
@@ -97,6 +110,7 @@ class DriveSystem(object):
         at the given speed for the given number of inches,
         using the encoder (degrees traveled sensor) built into the motors.
         """
+
 
     # -------------------------------------------------------------------------
     # Methods for driving that use the color sensor.
