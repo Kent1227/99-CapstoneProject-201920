29017175e2a2f540771e2af01c0f55009146542d
smithkg <smithkg@rose-hulman.edu>
2019-02-20 13:39:57 -0500

    updated

--- src/m1_extra.py
+++ src/m1_extra.py
@@ -5,9 +5,11 @@ def disco(robot):
     """
     :type robot: rosebot.RoseBot
     """
-    while robot.sensor_system.color_sensor.get_color() is not "blue":
+    while True:
         robot.drive_system.go(100, 100)
         if robot.sensor_system.ir_proximity_sensor.get_distance() < 5:
+            break
+        if robot.sensor_system.color_sensor.get_color_as_name() is "red":
             break
     robot.drive_system.pivot_left(50, 2)
     robot.arm_and_claw.raise_arm()
@@ -17,6 +19,7 @@ def disco(robot):
     robot.arm_and_claw.raise_arm()
     robot.drive_system.pivot_right(50, 2)
     robot.arm_and_claw.lower_arm()
+    robot.drive_system.stop()
 
 
 def shake(robot):
@@ -25,9 +28,11 @@ def shake(robot):
     :param robot:
     :return:
     """
-    while robot.sensor_system.color_sensor.get_color() is not "blue":
+    while True:
         robot.drive_system.go(100, 100)
         if robot.sensor_system.ir_proximity_sensor.get_distance() < 5:
+            break
+        if robot.sensor_system.color_sensor.get_color_as_name() is "red":
             break
     robot.drive_system.spin_clockwise_until_sees_object(100, 1)
     robot.drive_system.spin_counterclockwise_until_sees_object(100, 1)
@@ -41,6 +46,7 @@ def shake(robot):
     robot.drive_system.spin_counterclockwise_until_sees_object(100, 1)
     robot.drive_system.spin_clockwise_until_sees_object(100, 1)
     robot.drive_system.spin_counterclockwise_until_sees_object(100, 1)
+    robot.drive_system.stop()
 
 
 def spin(robot):
@@ -49,11 +55,14 @@ def spin(robot):
     :param robot:
     :return:
     """
-    while robot.sensor_system.color_sensor.get_color() is not "blue":
+    while True:
         robot.drive_system.go(100, 100)
         if robot.sensor_system.ir_proximity_sensor.get_distance() < 5:
             break
+        if robot.sensor_system.color_sensor.get_color_as_name() is "red":
+            break
     robot.drive_system.spin_clockwise_until_sees_object(100, 1000000000)
+    robot.drive_system.stop()
 
 
 def raise_the_roof(robot):
@@ -62,10 +71,13 @@ def raise_the_roof(robot):
     :param robot:
     :return:
     """
-    while robot.sensor_system.color_sensor.get_color() is not "blue":
+    while True:
         robot.drive_system.go(100, 100)
         if robot.sensor_system.ir_proximity_sensor.get_distance() < 5:
+            break
+        if robot.sensor_system.color_sensor.get_color_as_name() is "red":
             break
     robot.arm_and_claw.calibrate_arm()
     robot.arm_and_claw.raise_arm()
     robot.arm_and_claw.lower_arm()
+    robot.drive_system.stop()
